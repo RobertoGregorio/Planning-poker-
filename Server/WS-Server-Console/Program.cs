13f2babaf9870418bec21e23ee7d9037b7f9895b
Roberto Gregorio <roberto.gregorio@accessone.com.br>
2024-04-03 23:38:42 -0300

    Projeto de chat socket

---
+++ Server/WS-Server-Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using Fleck;
+
+var socketServer = new WebSocketServer(location: "ws//0.0.0.0:8181");
+
+socketServer.Start(connection =>
+{
+    Console.WriteLine("Sever socket ON");
+    connection.OnMessage = message =>
+    {
+        Console.WriteLine(message);
+    };
+});
+
+
+Console.ReadKey();
+
---
+++ Server/WS-Server/Program.cs
@@ -0,0 +1,13 @@
+using Fleck;
+
+
+var server = new WebSocketServer("ws://0.0.0.0:8181");
+server.Start(socket =>
+{
+    socket.OnOpen = () => Console.WriteLine("Open!");
+    socket.OnClose = () => Console.WriteLine("Close!");
+    socket.OnMessage = message => socket.Send(message);
+});
+
+
+WebApplication.CreateBuilder(args).Build().Run();
